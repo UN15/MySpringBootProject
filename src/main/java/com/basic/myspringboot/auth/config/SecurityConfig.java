6edd61100da204bb3e18bf026f9f2770a563907d
정재운 <jaeun101501@gmail.com>
2025-05-12 15:59:34 +0900

    auth add

--- src/main/java/com/basic/myspringboot/auth/config/SecurityConfig.java
+++ src/main/java/com/basic/myspringboot/auth/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.basic.myspringboot.auth.config;
 
+import com.basic.myspringboot.auth.service.UserInfoUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -24,32 +28,50 @@ public class SecurityConfig {
     }
 
     @Bean
-    //authentication
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("adminboot")
-                .password(encoder.encode("pwd1"))
-                .roles("ADMIN") // 관리자
-                .build();
-        UserDetails user = User.withUsername("userboot")
-                .password(encoder.encode("pwd2"))
-                .roles("USER") //사용자
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-    }
-
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //csrf(권한 도용해 원치않는 요청 보내는 공격)는 disable 실제 운영환경에서는 활성화
         return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
                     // /api/users/welcome 패스는 인증 없이 접근 가능한 경로
-                    auth.requestMatchers("/api/users/welcome").permitAll()
-                            // /api/users/** welcome 뺀 몯든 패스ㄴ는 인증 후에만 접근 가능한 경로
+                    // /userinfos/new 패스는 user_info 엔티티를 추가하는 경로는 인증 필요 없음
+                    auth.requestMatchers("/api/users/welcome", "/userinfos/new").permitAll()
+                            // /api/users/** welcome 뺀 모든 패스는 인증 후에만 접근 가능한 경로
                             .requestMatchers("/api/users/**").authenticated();
                 })
                 //스프링이 제공한 인증 Form을 사용함
                 .formLogin(withDefaults())
                 .build();
     }
+
+    //개발자가 커스텀한 UserServiceDetail 서비스를 SpringBean으로 등록하기
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserInfoUserDetailsService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        //UserServiceDetail 서비스가 어떤 객체인지 알려주기
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        //패스워드 인코더가 어떤 객체인지 알려주기
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+//    @Bean
+//    //authentication
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        UserDetails admin = User.withUsername("adminboot")
+//                .password(encoder.encode("pwd1"))
+//                .roles("ADMIN") // 관리자
+//                .build();
+//        UserDetails user = User.withUsername("userboot")
+//                .password(encoder.encode("pwd2"))
+//                .roles("USER") //사용자
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
 
 }
